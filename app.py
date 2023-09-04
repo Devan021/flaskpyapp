aa6127bf62c366df7743b16626ee436aefc024b9
Devan021 <devansp2020@gmail.com>
2023-09-04 12:38:45 +0530

    added OOP

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
+api = Api(app)
 
 # MongoDB Configuration
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/Users'
@@ -12,48 +14,76 @@ db = mongo.get_database()
 # User Collection
 users_collection = db.users
 
-# Define User Schema (Optional: You can use Marshmallow for validation)
-class UserSchema:
+# Define User Schema
+class UserSchema(object):
     def __init__(self, name, email, password):
         self.name = name
         self.email = email
         self.password = password
 
-# CRUD Endpoints
+    @classmethod
+    def validate(cls, data):
+        errors = {}
+        if not data['name']:
+            errors['name'] = 'Name is required'
+        if not data['email']:
+            errors['email'] = 'Email is required'
+        if not data['password']:
+            errors['password'] = 'Password is required'
+        return errors
 
-@app.route('/users', methods=['GET'])
-def get_all_users():
-    users = list(users_collection.find())
-    return jsonify(users), 200
+# Helper function to convert ObjectId to string
+def jsonify_user(user):
+    user['_id'] = str(user['_id'])
+    return user
 
-@app.route('/users/<string:id>', methods=['GET'])
-def get_user(id):
-    user = users_collection.find_one({'_id': ObjectId(id)})
-    if user:
-        return jsonify(user), 200
-    return jsonify({'message': 'User not found'}), 404
+# Create a parser for handling request data
+user_parser = reqparse.RequestParser()
+user_parser.add_argument('name', type=str, required=True)
+user_parser.add_argument('email', type=str, required=True)
+user_parser.add_argument('password', type=str, required=True)
 
-@app.route('/users', methods=['POST'])
-def create_user():
-    data = request.get_json()
-    user = UserSchema(data['name'], data['email'], data['password'])
-    result = users_collection.insert_one(user.__dict__)
-    return jsonify({'message': 'User created', 'id': str(result.inserted_id)}), 201
+# Define a User resource class
+class UserResource(Resource):
+    def get(self, id=None):
+        if id:
+            user = users_collection.find_one({'_id': ObjectId(id)})
+            if user:
+                return jsonify(jsonify_user(user)), 200
+            return jsonify({'message': 'User not found'}), 404
+        else:
+            users = list(users_collection.find())
+            # Convert ObjectId to string for each user
+            users = [jsonify_user(user) for user in users]
+            return jsonify(users), 200
 
-@app.route('/users/<string:id>', methods=['PUT'])
-def update_user(id):
-    data = request.get_json()
-    result = users_collection.update_one({'_id': ObjectId(id)}, {'$set': data})
-    if result.modified_count > 0:
-        return jsonify({'message': 'User updated'}), 200
-    return jsonify({'message': 'User not found'}), 404
+    def post(self):
+        args = user_parser.parse_args()
+        errors = UserSchema.validate(args)
+        if errors:
+            return jsonify(errors), 400
+        user = UserSchema(args['name'], args['email'], args['password'])
+        result = users_collection.insert_one(user.__dict__)
+        return jsonify({'message': 'User created', 'id': str(result.inserted_id)}), 201
 
-@app.route('/users/<string:id>', methods=['DELETE'])
-def delete_user(id):
-    result = users_collection.delete_one({'_id': ObjectId(id)})
-    if result.deleted_count > 0:
-        return jsonify({'message': 'User deleted'}), 200
-    return jsonify({'message': 'User not found'}), 404
+    def put(self, id):
+        args = user_parser.parse_args()
+        errors = UserSchema.validate(args)
+        if errors:
+            return jsonify(errors), 400
+        result = users_collection.update_one({'_id': ObjectId(id)}, {'$set': args})
+        if result.modified_count > 0:
+            return jsonify({'message': 'User updated'}), 200
+        return jsonify({'message': 'User not found'}), 404
+
+    def delete(self, id):
+        result = users_collection.delete_one({'_id': ObjectId(id)})
+        if result.deleted_count > 0:
+            return jsonify({'message': 'User deleted'}), 200
+        return jsonify({'message': 'User not found'}), 404
+
+# Add the User resource to the API with the '/users' endpoint
+api.add_resource(UserResource, '/users', '/users/<string:id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
